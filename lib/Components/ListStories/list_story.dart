cba2ce02adba6f27f891f159dfe514a08cf66ed6
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-10-11 20:08:35 -0300

    add stories status and button add story

--- lib/Components/ListStories/list_story.dart
+++ lib/Components/ListStories/list_story.dart
@@ -1,6 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:facebook_clone/Components/PerfilImage/perfil_image.dart';
+import 'package:facebook_clone/Data/data.dart';
 import 'package:facebook_clone/Models/Stories/story.dart';
 import 'package:facebook_clone/Models/Users/user.dart';
+import 'package:facebook_clone/utils/color.dart';
 import 'package:flutter/material.dart';
 
 class StoryList extends StatelessWidget {
@@ -18,10 +21,20 @@ class StoryList extends StatelessWidget {
       child: ListView.builder(
           padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
           scrollDirection: Axis.horizontal,
-          itemCount: stories.length,
-          itemBuilder: (context, indice) {
-            Story story = stories[indice];
+          itemCount: 1 + stories.length,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              Story storyUser =
+                  Story(user: currentUser, urlImage: currentUser.urlImage);
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 4),
+                child: CardStory(
+                  stories: storyUser,
+                ),
+              );
+            }
 
+            Story story = stories[index - 1];
             return Padding(
               padding: EdgeInsets.symmetric(horizontal: 4),
               child: CardStory(
@@ -35,7 +48,10 @@ class StoryList extends StatelessWidget {
 
 class CardStory extends StatelessWidget {
   final Story stories;
-  const CardStory({Key? key, required this.stories}) : super(key: key);
+  final bool addStory;
+
+  const CardStory({Key? key, required this.stories, this.addStory = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +65,48 @@ class CardStory extends StatelessWidget {
             width: 110,
             fit: BoxFit.cover,
           ),
-        )
+        ),
+        Container(
+          height: double.infinity,
+          width: 110,
+          decoration: BoxDecoration(
+              gradient: ColorsPalet.gradientStory,
+              borderRadius: BorderRadius.circular(12)),
+        ),
+        Positioned(
+            top: 8,
+            left: 8,
+            child: addStory
+                ? Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.white, shape: BoxShape.circle),
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: Icon(Icons.add),
+                      iconSize: 30,
+                      color: ColorsPalet.facebookColor,
+                      onPressed: () {},
+                    ),
+                  )
+                : PerfilImage(
+                    urlImage: stories.user.urlImage,
+                    visualized: stories.visualized,
+                  )),
+        Positioned(
+            bottom: 8,
+            left: 8,
+            right: 8,
+            child: Text(
+              addStory ? "Criar Story" : stories.user.name,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ))
       ],
     );
   }
